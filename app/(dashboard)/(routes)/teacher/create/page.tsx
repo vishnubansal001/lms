a2f6441ad93e63308095512f829cfae4a3af8e95
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-10-13 15:49:07 +0530

    new:added the toast provider and the confetti provider

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -1,51 +1,51 @@
 "use client";
 
-// import * as z from "zod";
-// import axios from "axios";
-// import { zodResolver } from "@hookform/resolvers/zod";
-// import { useForm } from "react-hook-form";
+import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
-// import toast from "react-hot-toast";
+import toast from "react-hot-toast";
 
-// import {
-//   Form,
-//   FormControl,
-//   FormDescription,
-//   FormField,
-//   FormLabel,
-//   FormMessage,
-//   FormItem,
-// } from "@/components/ui/form";
-// import { Button } from "@/components/ui/button";
-// import { Input } from "@/components/ui/input";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormLabel,
+  FormMessage,
+  FormItem,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
-// const formSchema = z.object({
-//   title: z.string().min(1, {
-//     message: "Title is required",
-//   }),
-// });
+const formSchema = z.object({
+  title: z.string().min(1, {
+    message: "Title is required",
+  }),
+});
 
 const CreatePage = () => {
   const router = useRouter();
-//   const form = useForm<z.infer<typeof formSchema>>({
-//     resolver: zodResolver(formSchema),
-//     defaultValues: {
-//       title: ""
-//     },
-//   });
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: ""
+    },
+  });
 
-//   const { isSubmitting, isValid } = form.formState;
+  const { isSubmitting, isValid } = form.formState;
 
-//   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-//     try {
-//       const response = await axios.post("/api/courses", values);
-//       router.push(`/teacher/courses/${response.data.id}`);
-//       toast.success("Course created");
-//     } catch {
-//       toast.error("Something went wrong");
-//     }
-//   }
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const response = await axios.post("/api/courses", values);
+      router.push(`/teacher/courses/${response.data.id}`);
+      toast.success("Course created");
+    } catch {
+      toast.error("Something went wrong");
+    }
+  }
 
   return ( 
     <div className="max-w-5xl mx-auto flex md:items-center md:justify-center h-full p-6">
@@ -56,7 +56,7 @@ const CreatePage = () => {
         <p className="text-sm text-slate-600">
           What would you like to name your course? Don&apos;t worry, you can change this later.
         </p>
-        {/* <Form {...form}>
+        <Form {...form}>
           <form
             onSubmit={form.handleSubmit(onSubmit)}
             className="space-y-8 mt-8"
@@ -100,7 +100,7 @@ const CreatePage = () => {
               </Button>
             </div>
           </form>
-        </Form> */}
+        </Form>
       </div>
     </div>
    );
