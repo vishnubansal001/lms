a159f299567b5e30ffe7be09c306b631cb117006
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-10-13 18:46:44 +0530

    new:added the compoenents

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -9,7 +9,7 @@ import { IconBadge } from "@/components/icon-badge";
 import { TitleForm } from "./_components/title-form";
 import { DescriptionForm } from "./_components/description-form";
 import { ImageForm } from "./_components/image-form";
-// import { CategoryForm } from "./_components/category-form";
+import { CategoryForm } from "./_components/category-form";
 // import { PriceForm } from "./_components/price-form";
 // import { AttachmentForm } from "./_components/attachment-form";
 // import { ChaptersForm } from "./_components/chapters-form";
@@ -61,7 +61,7 @@ const CourseIdPage = async ({
     course.imageUrl,
     course.price,
     course.categoryId,
-    // course.chapters.some(chapter => chapter.isPublished),
+    course.chapters.some(chapter => chapter.isPublished),
   ];
 
   const totalFields = requiredFields.length;
@@ -114,14 +114,14 @@ const CourseIdPage = async ({
               initialData={course}
               courseId={course.id}
             />
-            {/*<CategoryForm
+            <CategoryForm
               initialData={course}
               courseId={course.id}
               options={categories.map((category) => ({
                 label: category.name,
                 value: category.id,
               }))} 
-            />*/}
+            />
           </div>
           <div className="space-y-6">
             <div>
